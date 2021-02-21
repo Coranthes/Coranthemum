db4c8400ab248963912f404e9a8a6e8e201a666c
Coranthes <josa23oreo@gmail.com>
2021-02-21 15:40:55 -0500

    FIX ME

---
+++ src/main/java/coranthes/coranthemum/block/VariantStairsBlock.java
@@ -0,0 +1,33 @@
+package coranthes.coranthemum.block;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+import svenhjol.charm.base.CharmModule;
+import svenhjol.charm.base.block.ICharmBlock;
+import svenhjol.charm.base.enums.IVariantMaterial;
+
+public class VariantStairsBlock extends StairsBlock implements ICharmBlock {
+    private final CharmModule module;
+
+    public VariantStairsBlock(CharmModule module, IVariantMaterial type) {
+        super(AbstractBlock.Settings.copy(Blocks.STAIRS));
+        register(module, type.asString() + "_stairs");
+
+        this.module = module;
+    }
+
+    @Override
+    public void addStacksForDisplay(ItemGroup group, DefaultedList<ItemStack> list) {
+        if (enabled())
+            super.addStacksForDisplay(group, list);
+    }
+
+    @Override
+    public boolean enabled() {
+        return module.enabled;
+    }
+}
